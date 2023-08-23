4223a142167d4c0423370df5681d0f985c68895c
WitchLosser <anton.kozak230@gmail.com>
2023-08-23 23:14:30 +0300

    Make good CRUD and CORS policy

--- src/main/java/org/example/Controller/CategoryController.java
+++ src/main/java/org/example/Controller/CategoryController.java
@@ -3,8 +3,10 @@ package org.example.Controller;
 
 import lombok.AllArgsConstructor;
 import org.example.dto.category.CategoryCreateDTO;
+import org.example.dto.category.CategoryItemDTO;
 import org.example.dto.category.CategoryUpdateDTO;
 import org.example.entities.CategoryEntity;
+import org.example.mappers.CategoryMapper;
 import org.example.repositories.CategoryRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +18,10 @@ import java.util.Optional;
 public class CategoryController {
 
     private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
 
     @PostMapping("/category")
-    public CategoryEntity create(@RequestBody CategoryCreateDTO dto) {
+    public CategoryItemDTO createCategory(@RequestBody CategoryCreateDTO dto) {
         CategoryEntity cat = CategoryEntity
                 .builder()
                 .name(dto.getName())
@@ -26,30 +29,32 @@ public class CategoryController {
                 .image(dto.getImage())
                 .build();
         categoryRepository.save(cat);
-        return cat;
+        return categoryMapper.categoryToItemDTO(cat);
     }
+
     @GetMapping("/category/{id}")
-    public ResponseEntity<CategoryEntity> getCategoryById(@PathVariable int id) {
+    public ResponseEntity<CategoryItemDTO> getCategoryById(@PathVariable int id) {
         Optional<CategoryEntity> categoryOptional = categoryRepository.findById(id);
         return categoryOptional
-                .map(category -> ResponseEntity.ok().body(category))
+                .map(category -> ResponseEntity.ok().body(categoryMapper.categoryToItemDTO(category)))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/category")
-    public List<CategoryEntity> getAllCategories() {
-        return categoryRepository.findAll();
+    public List<CategoryItemDTO> getAllCategories() {
+        List<CategoryEntity> categoryList = categoryRepository.findAll();
+        return categoryMapper.listCategoriesToItemDTO(categoryList);
     }
 
     @PutMapping("/category/{id}")
-    public ResponseEntity<CategoryEntity> updateCategory(@PathVariable int id, @RequestBody CategoryUpdateDTO dto) {
+    public ResponseEntity<CategoryItemDTO> updateCategory(@PathVariable int id, @RequestBody CategoryUpdateDTO dto) {
         Optional<CategoryEntity> categoryOptional = categoryRepository.findById(id);
         return categoryOptional.map(category -> {
             category.setName(dto.getName());
             category.setDescription(dto.getDescription());
             category.setImage(dto.getImage());
             categoryRepository.save(category);
-            return ResponseEntity.ok().body(category);
+            return ResponseEntity.ok().body(categoryMapper.categoryToItemDTO(category));
         }).orElse(ResponseEntity.notFound().build());
     }
 
