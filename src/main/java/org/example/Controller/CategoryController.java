ab82d0ff4733a51dca3db8f1772019f0d645f73c
WitchLosser <anton.kozak230@gmail.com>
2023-08-26 23:52:46 +0300

    Add file logic

--- src/main/java/org/example/Controller/CategoryController.java
+++ src/main/java/org/example/Controller/CategoryController.java
@@ -1,17 +1,25 @@
 package org.example.Controller;
 
-
 import lombok.AllArgsConstructor;
-import org.example.dto.category.CategoryCreateDTO;
-import org.example.dto.category.CategoryItemDTO;
-import org.example.dto.category.CategoryUpdateDTO;
 import org.example.entities.CategoryEntity;
 import org.example.mappers.CategoryMapper;
 import org.example.repositories.CategoryRepository;
+import org.example.utils.ImageUtil;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.UUID;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Optional;
+import org.example.dto.category.*;
 
 @RestController
 @AllArgsConstructor
@@ -20,13 +28,38 @@ public class CategoryController {
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
 
-    @PostMapping("/category")
-    public CategoryItemDTO createCategory(@RequestBody CategoryCreateDTO dto) {
-        CategoryEntity cat = CategoryEntity
-                .builder()
-                .name(dto.getName())
-                .description(dto.getDescription())
-                .image(dto.getImage())
+    private static final String UPLOAD_DIR = "uploads";
+    private static final String RESIZED_DIR = "resized";
+
+    @PostMapping(value = "/category", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public CategoryItemDTO createCategory(
+            @RequestParam("name") String name,
+            @RequestParam("description") String description,
+            @RequestParam("image") MultipartFile imageFile) throws IOException {
+
+        String imageName = StringUtils.cleanPath(imageFile.getOriginalFilename());
+        String uniqueImageName = generateUniqueFileName(imageName);
+        Path uploadPath = Path.of(UPLOAD_DIR).toAbsolutePath().normalize();
+        Path imagePath = uploadPath.resolve(uniqueImageName);
+
+        Files.createDirectories(uploadPath);
+        Files.copy(imageFile.getInputStream(), imagePath, StandardCopyOption.REPLACE_EXISTING);
+
+        // Save resized images
+        Path resizedPath = Path.of(RESIZED_DIR).toAbsolutePath().normalize();
+        Files.createDirectories(resizedPath);
+
+
+        File resized150x150 = resizedPath.resolve( "150x150_" + uniqueImageName).toFile();
+        ImageUtil.resizeImage(imagePath.toFile(), resized150x150, 150, 150);
+
+        File resized300x300 = resizedPath.resolve( "300x300_" + uniqueImageName).toFile();
+        ImageUtil.resizeImage(imagePath.toFile(), resized300x300, 300, 300);
+
+        CategoryEntity cat = CategoryEntity.builder()
+                .name(name)
+                .description(description)
+                .image(uniqueImageName)
                 .build();
         categoryRepository.save(cat);
         return categoryMapper.categoryToItemDTO(cat);
@@ -46,13 +79,71 @@ public class CategoryController {
         return categoryMapper.listCategoriesToItemDTO(categoryList);
     }
 
-    @PutMapping("/category/{id}")
-    public ResponseEntity<CategoryItemDTO> updateCategory(@PathVariable int id, @RequestBody CategoryUpdateDTO dto) {
+    @PutMapping(value = "/category/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<CategoryItemDTO> updateCategory(
+            @PathVariable int id,
+            @RequestParam("name") String name,
+            @RequestParam("description") String description,
+            @RequestParam(value = "image", required = false) MultipartFile newImageFile) throws IOException {
         Optional<CategoryEntity> categoryOptional = categoryRepository.findById(id);
         return categoryOptional.map(category -> {
-            category.setName(dto.getName());
-            category.setDescription(dto.getDescription());
-            category.setImage(dto.getImage());
+            category.setName(name);
+            category.setDescription(description);
+
+            if (newImageFile != null && !newImageFile.isEmpty()) {
+                    // Delete associated image files
+                    String imagePathDelete = category.getImage();
+                    if (imagePathDelete != null) {
+                        Path imageFilePathDelete = Path.of(UPLOAD_DIR,imagePathDelete);
+                        Path resized150x150FilePath = Path.of(RESIZED_DIR, "150x150_" + imageFilePathDelete.getFileName());
+                        Path resized300x300FilePath = Path.of(RESIZED_DIR, "300x300_" + imageFilePathDelete.getFileName());
+
+                        try {
+                            Files.deleteIfExists(imageFilePathDelete);
+                            Files.deleteIfExists(resized150x150FilePath);
+                            Files.deleteIfExists(resized300x300FilePath);
+                        } catch (IOException e) {
+                            // Handle the exception if necessary
+                            e.printStackTrace();
+                        }
+                    }
+                    //add new photos
+                String imageName = StringUtils.cleanPath(newImageFile.getOriginalFilename());
+                String uniqueImageName = generateUniqueFileName(imageName);
+                Path uploadPath = Path.of(UPLOAD_DIR).toAbsolutePath().normalize();
+                Path imagePath = uploadPath.resolve(uniqueImageName);
+
+                try {
+                    Files.copy(newImageFile.getInputStream(), imagePath, StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // Save resized images with unique names
+                Path resizedPath = Path.of(RESIZED_DIR).toAbsolutePath().normalize();
+                try {
+                    Files.createDirectories(resizedPath);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                File resized150x150 = resizedPath.resolve("150x150_" + uniqueImageName).toFile();
+                try {
+                    ImageUtil.resizeImage(imagePath.toFile(), resized150x150, 150, 150);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                File resized300x300 = resizedPath.resolve( "300x300_" + uniqueImageName).toFile();
+                try {
+                    ImageUtil.resizeImage(imagePath.toFile(), resized300x300, 300, 300);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                category.setImage(uniqueImageName);
+            }
+
             categoryRepository.save(category);
             return ResponseEntity.ok().body(categoryMapper.categoryToItemDTO(category));
         }).orElse(ResponseEntity.notFound().build());
@@ -62,10 +153,34 @@ public class CategoryController {
     public ResponseEntity<Void> deleteCategory(@PathVariable int id) {
         Optional<CategoryEntity> categoryOptional = categoryRepository.findById(id);
         if (categoryOptional.isPresent()) {
+            CategoryEntity category = categoryOptional.get();
+
+            // Delete associated image files
+            String imagePath = category.getImage();
+            if (imagePath != null) {
+                Path imageFilePath = Path.of(UPLOAD_DIR,imagePath);
+                Path resized150x150FilePath = Path.of(RESIZED_DIR, "150x150_" + imageFilePath.getFileName());
+                Path resized300x300FilePath = Path.of(RESIZED_DIR, "300x300_" + imageFilePath.getFileName());
+
+                try {
+                    Files.deleteIfExists(imageFilePath);
+                    Files.deleteIfExists(resized150x150FilePath);
+                    Files.deleteIfExists(resized300x300FilePath);
+                } catch (IOException e) {
+                    // Handle the exception if necessary
+                    e.printStackTrace();
+                }
+            }
+
             categoryRepository.deleteById(id);
             return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+    private String generateUniqueFileName(String originalFileName) {
+        String extension = StringUtils.getFilenameExtension(originalFileName);
+        String uniqueFileName = UUID.randomUUID().toString() + "." + extension;
+        return uniqueFileName;
     }
 }
